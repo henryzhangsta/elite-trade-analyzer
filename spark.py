f1e54b261b221bdd80ab55886b9924e63ec81ef3
Henry Zhang <huadianz@gmail.com>
2015-01-18 13:29:03 -0800

    Fix arguments

--- spark.py
+++ spark.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     conf = SparkConf().setMaster('local[8]')
     sc = SparkContext(conf=conf)
 
-    systems = CSVLoader('data/headers/System.hcsv').loadMap()
-    stationCommodities = CSVLoader('data/headers/SC.hcsv').loadMap()
-    commodities = CSVLoader('data/headers/Commod.hcsv').loadMap()
+    systems = CSVLoader('data/headers/System.hcsv').loadMap(sc, 'data/System.csv')
+    stationCommodities = CSVLoader('data/headers/SC.hcsv').loadMap(sc, 'data/SC.csv')
+    commodities = CSVLoader('data/headers/Commod.hcsv').loadMap(sc, 'data/Commod.csv')
