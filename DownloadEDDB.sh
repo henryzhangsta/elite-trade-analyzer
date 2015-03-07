47b2a7362282aac6845d706433c03f043cde04e9
Fred Deschenes <tifred2004@gmail.com>
2015-03-07 10:58:34 -0500

    Now uses V2 of EDDB API

--- DownloadEDDB.sh
+++ DownloadEDDB.sh
@@ -1,3 +1,3 @@
 curl -o data/commodities.json http://eddb.io/archive/commodities.json
-curl -o data/systems.json http://eddb.io/archive/systems.json
-curl -o data/stations.json http://eddb.io/archive/stations.json
+curl -o data/systems.json http://eddb.io/archive/v2/systems.json
+curl -o data/stationsv1.json http://eddb.io/archive/stations.json
